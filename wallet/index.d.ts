1039ae9276e1b6bcd2ed21cee6e54e9dcfc8de64
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2020-08-12 15:00:47 +0100

    a5d7fff

---
+++ wallet/index.d.ts
@@ -0,0 +1,33 @@
+/// <reference types="node" />
+import { Network } from 'liquidjs-lib';
+export declare const EXPLORER_URL: {
+    liquid: string;
+    regtest: string;
+};
+export default class LiquidWallet {
+    scriptPubKey: string;
+    blindingKey?: string;
+    address: string;
+    network: Network;
+    constructor(identity: string, network: Network, blindingKey?: string);
+    static createTx(): string;
+    static isValidMnemonic(m: string): boolean;
+    static isValidWIF(wif: string, network: Network): boolean;
+    decodeTx(psbtBase64: string): any;
+    updateTx(psbtBase64: string, inputs: Array<any>, outputs: Array<any>): string;
+    signPsbtWithMnemonic(psbtBase64: string, mnemonic: string): string;
+    signPsbtWithPrivateKey(psbtBase64: string, wif: string): string;
+    private sign;
+}
+export declare function fetchUtxos(address: string, url: string): Promise<any>;
+export declare function unblindUtxos(utxos: Array<any>, blindingKey: string, url: string): Promise<{
+    asset: string;
+    txid: any;
+    vout: any;
+    value: string;
+    valueBlindingFactor: Buffer;
+    assetBlindingFactor: Buffer;
+}[]>;
+export declare function faucet(address: string, url: string): Promise<any>;
+export declare function mint(address: string, quantity: number, url: string): Promise<any>;
+export declare function fetchBalances(address: string, url: string, blindingKey?: string): Promise<any>;
